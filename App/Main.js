557af3df4edbed02bbb9b5bf26f024e9fd5b82b5
Sandip <sandip.kumar.kundu@pwc.com>
2018-04-02 15:49:16 +0530

    main Setup

--- App/Main.js
+++ App/Main.js
@@ -1,14 +1,24 @@
 "use strict";
-exports.__esModule = true;
-var express_1 = require("express");
+(async function(){
+var express = require("express");
+var Postgres=require("./Modules/Postgress")
+
 var process_1 = require("process");
-var app = express_1(); // Creating express object
-var router = express_1.Router(); //Initialising express router
-// app.use(express.static(__dirname + '/public'));//Making public folder visible in browser tree
-// app.use(Helmet());
+var path=require('path');
+var app = express(); // Creating express object
+var router = express.Router(); //Initialising express router
+var bodyParser = require('body-parser');
+app.use('/Frontend',express.static(path.join(__dirname,'../','Frontend')));
+app.use(bodyParser.json());
+app.use(bodyParser.urlencoded({ extended: true }))
 router.get('/', function (req, res) {
-    res.send("Worked"); //as a reponse sendding index.html to client browser
+    res.sendFile(path.join(__dirname,'../','Frontend','Index.html')); //as a reponse sendding index.html to client browser
 });
-// console.log(process)
+app.post('/login',async function (req, res) {
+var data=await Postgres.insert(req.body);
+// console.log("Data Returned"+data)
+res.json(data);
+});
 app.use('/', router);
 app.listen(process_1.env.PORT || 3001);
+})();
--- App/Modules/Postgress.js
+++ App/Modules/Postgress.js
@@ -1,15 +1,16 @@
 "use strict";
-var DB={};
-(async function lon(){
+
+(async function(){
     const pg = require('pg');
     const connectionString ='postgres://postgres:test@localhost:5432/User';
     let client ;
- 
+    var DB={};
     DB.init=async ()=>{
         console.log("at Init");
    client = new pg.Client(connectionString);
    client.connect();
     }
+
    DB.destroy=async ()=>{
         await client.end();
     }
@@ -19,12 +20,13 @@ var DB={};
         var Object1=data;
         // console.log(Object1.name);
         //var string ='INSERT INTO public."Employee"("Emp") VALUES'+'('+Object1.Emp+')'
-        const text = 'INSERT INTO public."Employee"("Emp", "name") VALUES($1, $2) RETURNING *'
-        const values = [Object1.Emp,Object1.name];
-        console.log(values);
+        const text = 'INSERT INTO public."Employee"(name, "Password") VALUES($1, $2) RETURNING *'
+        const values = [Object1.username,Object1.password];
+        // console.log(values);
         const  query =await client.query(text, values);
-
-        console.log(query.rows);
+        const query1=await client.query('Select * from public."Employee"') ;
+    //    console.log(query1.rows);
+        return query1.rows;
         DB.destroy();
 
     }
@@ -34,11 +36,6 @@ var DB={};
 
 // const connectionString ='postgres://yswcedfh:25GoFvra5ZiytOJswH7bPjVlExuSs35s@horton.elephantsql.com:5432/yswcedfh';
 
-
-
-
- 
-
-module.export=DB;
+module.exports=DB;
  })();
- DB.insert({Emp:24,name:'Snigs'});
+ 
